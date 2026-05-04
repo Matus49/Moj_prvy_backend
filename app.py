b4aab0dc6a4b6271f348d1930075e759be185521
Matus49 <matus.bucko10@gmail.com>
2026-05-04 09:18:23 +0200

    Refactor app.py to use SQLAlchemy and fix API routes
    Refactor app.py to integrate SQLAlchemy and improve API functionality.

--- app.py
+++ app.py
@@ -1,130 +1,34 @@
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
-databaza = {
-    "students": [
-    {    
-        "id":1,
-        "name":"Matus",
-        "surname":"Bucko",
-        "nickname":"NEON1X",
-        "photo":"https://i.pravatar.cc/300?u=1",
-    },
-    {
-        "id":2,
-        "name":"Samo",
-        "surname":"Haring",
-        "nickname":"Topikar",
-        "photo":"https://i.pravatar.cc/300?u=2",
-    },
-    {
-        "id":3,
-        "name":"Milan",
-        "surname":"Kokina",
-        "nickname":"RED BULL",
-        "photo":"https://i.pravatar.cc/300?u=3",
-    },
-    {
-        "id":4,
-        "name":"Matej",
-        "surname":"Randziak",
-        "nickname":"Tankista",
-        "photo":"https://i.pravatar.cc/300?u=4",
-
-    },
-    {
-        "id":5,
-        "name":"Janka",
-        "surname":"Vargova",
-        "nickname":"Dzejna",
-        "photo":"https://i.pravatar.cc/300?u=5",
-
-    },
-    {
-        "id":6,
-        "name":"Martin",
-        "surname":"Jelinek",
-        "nickname":"",
-        "photo":"https://i.pravatar.cc/300?u=6",
-    
-
-    },
-        {
-        "id":7,
-        "name":"Markus",
-        "surname":"Martis",
-        "nickname":"Zid",
-        "photo":"https://i.pravatar.cc/300?u=7",
-    
-
-    },
-        {
-        "id":8,
-        "name":"Adrian",
-        "surname":"Cervenka",
-        "nickname":"Valorant Enjoyer",
-        "photo":"https://i.pravatar.cc/300?u=8",
-    
-
-    },
-        {
-        "id":9,
-        "name":"Tomas",
-        "surname":"Jurcak",
-        "nickname":"Jurcacik",
-        "photo":"https://i.pravatar.cc/300?u=9",
-    
-
-    },
-        {
-        "id":10,
-        "name":"Marko",
-        "surname":"Mihalicka",
-        "nickname":"Jiggler",
-        "photo":"https://i.pravatar.cc/300?u=10",
-    
-
-    },
-    ]
-}
-@app.route("/api")
-def api():
-    return jsonify((databaza))
-    
-# 1. Jednoduchý GET endpoint
-@app.route('/api/student/int:student_id>')
-def find_student(student_id):
-    for student in databaza["students"]:
-        if student["id"] == student_id:
-            return jsonify(student)
-
-    student = databaza["students"][student_id - 1]
-    return jsonify(student),
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
 
-# Konfigurácia databázy
+# Konfigurácia databázy pre správy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Model databázy
+# Model databázy pre správy (na spodku stránky)
 class Sprava(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     obsah = db.Column(db.String(200), nullable=False)
 
-# Vytvorenie databázy (pri prvom spustení)
+# Simulovaná databáza študentov
+databaza = {
+    "students": [
+        {"id": 1, "name": "Matus", "surname": "Bucko", "nickname": "NEON1X", "photo": "https://i.pravatar.cc/300?u=1"},
+        {"id": 2, "name": "Samo", "surname": "Haring", "nickname": "Topikar", "photo": "https://i.pravatar.cc/300?u=2"},
+        {"id": 3, "name": "Milan", "surname": "Kokina", "nickname": "RED BULL", "photo": "https://i.pravatar.cc/300?u=3"},
+        {"id": 4, "name": "Matej", "surname": "Randziak", "nickname": "Tankista", "photo": "https://i.pravatar.cc/300?u=4"},
+        {"id": 5, "name": "Janka", "surname": "Vargova", "nickname": "Dzejna", "photo": "https://i.pravatar.cc/300?u=5"},
+        {"id": 6, "name": "Martin", "surname": "Jelinek", "nickname": "N/A", "photo": "https://i.pravatar.cc/300?u=6"},
+        {"id": 7, "name": "Markus", "surname": "Martis", "nickname": "Zid", "photo": "https://i.pravatar.cc/300?u=7"},
+        {"id": 8, "name": "Adrian", "surname": "Cervenka", "nickname": "Valorant Enjoyer", "photo": "https://i.pravatar.cc/300?u=8"},
+        {"id": 9, "name": "Tomas", "surname": "Jurcak", "nickname": "Jurcacik", "photo": "https://i.pravatar.cc/300?u=9"},
+        {"id": 10, "name": "Marko", "surname": "Mihalicka", "nickname": "Jiggler", "photo": "https://i.pravatar.cc/300?u=10"},
+    ]
+}
+
 with app.app_context():
     db.create_all()
 
@@ -138,7 +42,16 @@ def index():
         return redirect('/')
     
     vsetky_spravy = Sprava.query.all()
-    return render_template('index.html', spravy=vsetky_spravy)
+    # Posielame študentov aj správy do HTML
+    return render_template('index.html', students=databaza["students"], spravy=vsetky_spravy)
+
+# API endpoint (opravený syntax)
+@app.route('/api/student/<int:student_id>')
+def find_student(student_id):
+    for student in databaza["students"]:
+        if student["id"] == student_id:
+            return jsonify(student)
+    return jsonify({"error": "Student not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
